4bc04b6f9050d80154c4de85f0c8ae0bb066b9ac
Kia Farhang <kfarhang0@gmail.com>
2017-09-26 17:08:02 -0500

    Initial commit - project might be legit!

---
+++ src/js/scroll.ts
@@ -0,0 +1,75 @@
+// const fetchPage = async (page: string): Promise<string> => {
+
+//     return await parseResponseText(await fetch(page));
+// }
+
+// const parseResponseText = async (response: Response): Promise<string> => {
+//     return await response.text();
+// }
+
+// const appendHTMLAfterElement = (element: Element, html: string): void => {
+//     const newHTML = document.createElement('div');
+//     newHTML.innerHTML = html;
+//     const parentElement = element.parentElement;
+//     parentElement.appendChild(newHTML);
+// }
+
+// // const replaceAllScripts = (document: Document): void => {
+// //     const scripts = Array.from(document.getElementsByTagName('script'));
+// //     scripts.forEach((script: HTMLScriptElement) => {
+// //         reloadElement(script, 'script');
+// //     });
+// // }
+
+// // const reloadElement = (element: Element, tagToCreate: string): void => {
+// //     const parent = element.parentElement;
+// //     const html = element.innerHTML;
+// //     parent.removeChild(element);
+
+// //     const newElement = document.createElement(tagToCreate);
+// //     newElement.innerHTML = html;
+// //     parent.appendChild(newElement);
+
+// // }
+
+// // const getScriptsInElement = (element: Element): NodeListOf<HTMLScriptElement> => {
+// //     return element.getElementsByTagName('script');
+// // }
+
+// const addHDNPlayer = (document: Document): void => {
+//     const script = document.createElement('script');
+//     script.src = 'https://content.jwplatform.com/libraries/uVzyVL6s.js';
+//     document.head.appendChild(script);
+// }
+
+// const createJWVideo = (window: Window): void => {
+//     if (window.jwplayer) {
+//         const container = document.getElementsByClassName('hearst-jwplayer')[0];
+//         const parent = container.parentElement;
+//         parent.removeChild(container);
+//         const newContainer = document.createElement('div');
+//         newContainer.id = 'jw-player-kia';
+//         parent.appendChild(newContainer);
+//         const playerInstance = jwplayer('jw-player-kia');
+//         playerInstance.setup({
+//             file: "http://content.jwplatform.com/videos/QOZNRGOD-DKEYSFrx.mp4"
+//         });
+//     }
+// }
+
+// const main = async (url: string): Promise<void> => {
+//     addHDNPlayer(document);
+//     const html = await fetchPage(url);
+//     const article = document.getElementById('content');
+//     console.log('appending page');
+//     await appendHTMLAfterElement(article, html);
+//     createJWVideo(window);
+//     // const logger = document.getElementById('dummyLog');
+//     // reloadElement(logger, 'script');
+//     // const div = document.getElementById('player-9eoq3cVV-container');
+//     // await reloadElement(div, 'div');
+
+//     // const scripts = getScriptsInElement(div);
+//     // scripts.forEach(script => reloadElement(script, 'script'));
+//     // replaceAllScripts(document);
+// }
