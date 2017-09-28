a230e59d2ab7d7477fc93007525ec22d023bee81
Kia Farhang <kfarhang0@gmail.com>
2017-09-28 17:57:21 -0500

    Added a function from SO to get percentage viewer has scrolled down the screen

--- src/js/utility.ts
+++ src/js/utility.ts
@@ -15,6 +15,7 @@ export const containsSelector = (element: Element, selector: string): boolean =>
 }
 
 export const fetchPageHTML = async (page: string): Promise<string> => {
+
     try {
         const response = await fetch(page);
         return await response.text();
@@ -41,6 +42,20 @@ export const deduplicateElementsOnAttribute = (elements: HTMLCollection | NodeLi
     })
     return filteredArray;
 }
+
+// Shamelessly stolen from Stack Overflow:
+// https://stackoverflow.com/questions/2387136/cross-browser-method-to-determine-vertical-scroll-percentage-in-javascript
+
+export const getScrollPercent = (document: Document) => {
+    const h = document.documentElement,
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+
+    const percent = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+    return percent;
+}
+
 
 
 // const appendHTMLAfterElement = (element: Element, html: string): void => {
